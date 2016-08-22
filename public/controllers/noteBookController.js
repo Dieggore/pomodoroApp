402415da0c14c6fbee8865c67886ce980747f770
dieggger <dieggger@gmail.com>
2016-08-22 17:35:07 -0500

    separated noteBook controller

--- public/controllers/noteBookController.js
+++ public/controllers/noteBookController.js
@@ -1,3 +1,19 @@
 /**
  * Created by dmontes on 8/17/16.
  */
+angular.module( 'pomodoroApp')
+    .controller( 'NoteBookController', [ noteBookController ]);
+
+function noteBookController (){
+    var vm = this;
+
+    vm.date = new Date();
+    vm.textAreas = [
+        { text:" " },
+        { text:" " },
+        { text:" " },
+        { text:" " },
+        { text:" " }
+    ];
+
+}
