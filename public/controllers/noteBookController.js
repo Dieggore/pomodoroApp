668f90adc1f04c8e92d117ce7e5f602e69f04470
dieggger <dieggger@gmail.com>
2016-08-24 16:42:56 -0500

    set sessionStorage for notebook textareas model

--- public/controllers/noteBookController.js
+++ public/controllers/noteBookController.js
@@ -5,15 +5,40 @@ angular.module( 'pomodoroApp')
     .controller( 'NoteBookController', [ noteBookController ]);
 
 function noteBookController (){
-    var vm = this;
 
-    vm.date = new Date();
-    vm.textAreas = [
-        { text:" " },
-        { text:" " },
-        { text:" " },
-        { text:" " },
-        { text:" " }
-    ];
+    var vm = this;
+    var storage = sessionStorage;
+    var storageNameSpace = "noteBookStorage";
+    var currentStorage = null;
+
+    storage[ storageNameSpace ] = storage[ storageNameSpace ] || null;
+    //=== binded variables
+
+    //====================
+
+    //=== public functions
+    vm.saveToSessionStorage = saveToSessionStorage;
+
+    //====================
+
+    if ( hasSomethingSavedInSessionStorage() ){
+        vm.textAreas = currentStorage;
+    } else {
+        vm.textAreas = [ {}, {}, {}, {}];
+    }
+
+    //===============
+    function saveToSessionStorage(){
+        storage[ storageNameSpace ] = JSON.stringify( vm.textAreas );
+    }
+
+    function hasSomethingSavedInSessionStorage(){
+        currentStorage = JSON.parse( storage[ storageNameSpace ] );
+        if( _.isObject( currentStorage ) ){
+            return true;
+        }
+        return false;
+    }
+
 
 }
